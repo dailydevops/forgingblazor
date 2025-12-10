da4ba35b988fb7016dd8bea078daca769777d215
Martin Stühmer <me@samtrion.net>
2025-12-10 22:41:35 +0100

    fix: Added missing method `IsServiceTypeRegistered`

---
+++ src/NetEvolve.ForgingBlazor/ServiceCollectionExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace NetEvolve.ForgingBlazor;
+
+using System.CommandLine;
+using System.Diagnostics.CodeAnalysis;
+using Microsoft.Extensions.DependencyInjection;
+using NetEvolve.ForgingBlazor.Commands;
+using NetEvolve.ForgingBlazor.Core.Models;
+using NetEvolve.ForgingBlazor.Extensibility.Abstractions;
+using NetEvolve.ForgingBlazor.Extensibility.Models;
+using NetEvolve.ForgingBlazor.Services;
+
+internal static class ServiceCollectionExtensions
+{
+    internal static bool IsServiceTypeRegistered<T>(this IServiceCollection builder)
+        where T : class => builder.Any(x => x.ServiceType == typeof(T));
+}
