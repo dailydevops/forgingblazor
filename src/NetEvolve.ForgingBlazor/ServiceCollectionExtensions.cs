dcdd1131afbf21aaf0c141dc67915591663260c5
Martin Stühmer <me@samtrion.net>
2025-12-10 22:45:56 +0100

    fix: Removed unnecessary namespaces

--- src/NetEvolve.ForgingBlazor/ServiceCollectionExtensions.cs
+++ src/NetEvolve.ForgingBlazor/ServiceCollectionExtensions.cs
@@ -1,13 +1,9 @@
 ﻿namespace NetEvolve.ForgingBlazor;
 
 using System.CommandLine;
-using System.Diagnostics.CodeAnalysis;
 using Microsoft.Extensions.DependencyInjection;
 using NetEvolve.ForgingBlazor.Commands;
-using NetEvolve.ForgingBlazor.Core.Models;
 using NetEvolve.ForgingBlazor.Extensibility.Abstractions;
-using NetEvolve.ForgingBlazor.Extensibility.Models;
-using NetEvolve.ForgingBlazor.Services;
 
 internal static class ServiceCollectionExtensions
 {
