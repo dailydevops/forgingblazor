f39f8a9570dcdd747b2d752ead5780a80afdad60
Martin Stühmer <me@samtrion.net>
2025-12-10 13:04:22 +0100

    test: Added first UnitTest

---
+++ tests/NetEvolve.ForgingBlazor.Tests.Unit/ForgingBlazorApplicationBuilderTests.cs
@@ -0,0 +1,16 @@
+﻿namespace NetEvolve.ForgingBlazor.Tests.Unit;
+
+public sealed class ForgingBlazorApplicationBuilderTests
+{
+    [Test]
+    public async Task RunAsync_WithoutArguments_ReturnsOne()
+    {
+        var args = Array.Empty<string>();
+        var builder = ForgingBlazorApplicationBuilder.CreateBuilder(args);
+        var app = builder.Build();
+
+        var exitCode = await app.RunAsync();
+
+        _ = await Assert.That(exitCode).IsEqualTo(1);
+    }
+}
