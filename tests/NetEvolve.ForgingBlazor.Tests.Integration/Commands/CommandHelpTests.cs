e4941eac282595eb51f0ff0e9691773fe60a950a
Martin Stühmer <me@samtrion.net>
2025-12-14 11:29:56 +0100

    test: Extended Command Help and Command Build tests

--- tests/NetEvolve.ForgingBlazor.Tests.Integration/Commands/CommandHelpTests.cs
+++ tests/NetEvolve.ForgingBlazor.Tests.Integration/Commands/CommandHelpTests.cs
@@ -13,15 +13,25 @@ public class CommandHelpTests
 
         var app = builder.Build();
         await using var output = new StringWriter();
+        await using var error = new StringWriter();
 
-        if (app is Application Application)
+        if (app is Application application)
         {
-            Application.InvocationConfiguration = new() { Output = output };
+            application.InvocationConfiguration = new() { Error = error, Output = output };
         }
 
         _ = await app.RunAsync().ConfigureAwait(false);
 
-        _ = await Verify(new { args, output }).DontIgnoreEmptyCollections().HashParameters();
+        _ = await Verify(
+                new
+                {
+                    args,
+                    error,
+                    output,
+                }
+            )
+            .DontIgnoreEmptyCollections()
+            .HashParameters();
     }
 
     public static IEnumerable<Func<string[]>> GetHelpArguments =>
